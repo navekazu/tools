0f28858fd181bcff8d65a5ff8bd64863812d63a7
navekazu <navekazu0927@gmail.com>
2016-01-26 11:09:23 +0900

    Tailするツールを作成中。

---
+++ Tail/src/main/java/tools/tail/App.java
@@ -0,0 +1,13 @@
+package tools.tail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
