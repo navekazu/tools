7e5feabc51bcd248a6bc662b66d2baade2b4d285
navekazu <navekazu0927@gmail.com>
2016-02-05 16:38:59 +0900

    指定ワードで検索をするブラウザ起動用JavaFXアプリ

---
+++ WebLauncher/src/main/java/tools/weblauncher/App.java
@@ -0,0 +1,15 @@
+package tools.weblauncher;
+
+
+import javafx.application.Application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Application.launch(MainController.class, args);
+    }
+}
