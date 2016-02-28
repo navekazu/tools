b38bc15c9189edeaa999b103ddcd797b1cf372b8
watanabegithub <navekazu0927@gmail.com>
2016-02-29 08:01:02 +0900

    イベントハンドラ追加。

--- DBConnector6/src/main/java/tools/dbconnector6/MainController.java
+++ DBConnector6/src/main/java/tools/dbconnector6/MainController.java
@@ -1,18 +1,25 @@
 package tools.dbconnector6;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import tools.dbconnector6.ui.QueryTextAreaController;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController extends Application implements Initializable {
+    private Stage primaryStage;
+
     @FXML
     private TextField filterTextField;
 
@@ -46,14 +53,20 @@ public class MainController extends Application implements Initializable {
     @FXML
     private TextArea logTextArea;
 
+    private QueryTextAreaController queryTextAreaController;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setTitle("DBConnector6");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        connect();
 
         // 初期フォーカスを検索ワード入力欄に（initializeの中ではフォーカス移動できない）
         MainController c = loader.getController();
@@ -66,6 +79,49 @@ public class MainController extends Application implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        queryTextAreaController = new QueryTextAreaController(this, queryTextArea);
+    }
 
+    @FXML
+    private void onConnect(ActionEvent event) {
+        connect();
+    }
+
+    @FXML
+    private void onDisconnect(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onCommit(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onRollback(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void onCancel(ActionEvent event) {
+
+    }
+
+    private void connect() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Connect.fxml"));
+            loader.load();
+            Parent root = loader.getRoot();
+            Scene scene = new Scene(root);
+            Stage confirmDialog = new Stage(StageStyle.UTILITY);
+            confirmDialog.setScene(scene);
+            confirmDialog.initOwner(primaryStage);
+            confirmDialog.initModality(Modality.APPLICATION_MODAL);
+            confirmDialog.setTitle("Connect");
+            confirmDialog.showAndWait(); // ダイアログが閉じるまでブロックされる
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
