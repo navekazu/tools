6a2851e0d1a5e1c154c0462b66230fb247670182
navekazu <navekazu0927@gmail.com>
2016-03-01 10:19:37 +0900

    Revert "docコメントを追加"
    This reverts commit fb212d42d4dd27b9bc61aefb905718a4e774098a.

--- DBConnector6/src/main/java/tools/dbconnector6/controller/ControllerManager.java
+++ DBConnector6/src/main/java/tools/dbconnector6/controller/ControllerManager.java
@@ -15,44 +15,21 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * コントローラ管理
- */
 public class ControllerManager {
-
-    /**
-     * コントローラ管理のシングルトンオブジェクト
-     */
     private static final ControllerManager singleton = new ControllerManager();
 
-    /**
-     * ステージ定義のマップ
-     */
     private static final Map<String, StageDefine> stageDefinedMap = new HashMap<>();
     static {
         stageDefinedMap.put("main", StageDefine.builder().fxml("/fxml/Main.fxml").title("DBConnector6").build());
         stageDefinedMap.put("connect", StageDefine.builder().fxml("/fxml/Connect.fxml").title("Connect").build());
     }
-
-    /**
-     * コントローラ管理のシングルトンオブジェクトを取得する
-     * @return コントローラ管理のシングルトンオブジェクト
-     */
     public static ControllerManager getControllerManager() {
         return singleton;
     }
 
-    /**
-     * アプリケーション起動時のステージ
-     */
+
     private Stage primaryStage;
 
-    /**
-     * メイン画面のステージを取得する
-     * @param primaryStage アプリケーション起動時のステージ
-     * @return メイン画面のFXMLを読み込んだアプリケーション起動時のステージ
-     * @throws IOException FXML読み込みに失敗した場合
-     */
     public Stage getMainStage(Stage primaryStage) throws IOException {
         this.primaryStage = primaryStage;
         StageDefine stageDefine = getStageDefine("main");
@@ -66,12 +43,6 @@ public class ControllerManager {
         return primaryStage;
     }
 
-    /**
-     * アプリケーション起動時のステージを親に持つ子のステージを取得する
-     * @param name ステージ定義のマップ名
-     * @return 子のステージ
-     * @throws IOException FXML読み込みに失敗した場合
-     */
     public Stage getSubStage(String name) throws IOException {
         StageDefine stageDefine = getStageDefine(name);
 
@@ -88,30 +59,15 @@ public class ControllerManager {
         return subStage;
     }
 
-    /**
-     * ステージ定義
-     */
     @AllArgsConstructor
     @Data
     @NoArgsConstructor
     @Builder
     private static class StageDefine {
-        /**
-         * FXMLファイル名
-         */
         private String fxml;
-
-        /**
-         * ステージのタイトル
-         */
         private String title;
     }
 
-    /**
-     * ステージ定義を取得する
-     * @param name ステージ定義名
-     * @return ステージ定義
-     */
     private StageDefine getStageDefine(String name) {
         if (!stageDefinedMap.containsKey(name)) {
             throw new IllegalArgumentException(String.format("'%s' is not found in stageDefinedMap.", name));
