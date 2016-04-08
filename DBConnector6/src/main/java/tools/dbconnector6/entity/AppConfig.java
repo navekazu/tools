d078673989e66b1f1df3e7c92168e8f69bbb8ac4
navekazu <navekazu0927@gmail.com>
2016-04-08 11:16:37 +0900

    javadoc修正。

--- DBConnector6/src/main/java/tools/dbconnector6/entity/AppConfig.java
+++ DBConnector6/src/main/java/tools/dbconnector6/entity/AppConfig.java
@@ -1,6 +1,17 @@
 package tools.dbconnector6.entity;
 
+/**
+ * アプリケーション設定ファイルのentity基底クラス。<br>
+ * ファイルは「~/.DBConnector6/config/app_config」として保存する。<br>
+ * アプリケーション設定ファイルは行の先頭にラベルがあり、そのラベルによってその行が何の情報を保持するかが決定する。<br>
+ */
 public abstract class AppConfig {
+
+    /**
+     * 何のアプリケーション設定ファイルかを表すラベル。<br>
+     * このクラスを拡張する具象クラスが実装する必要がある。<br>
+     * @return ラベル文字列
+     */
     public static String getLabel() {
         return "";
     }
