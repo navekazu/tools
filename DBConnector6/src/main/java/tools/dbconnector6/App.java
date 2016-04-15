0b850b75211b2b9f605b0eb1f6348edb3a6ff290
navekazu <navekazu0927@gmail.com>
2016-04-15 15:46:12 +0900

    docコメントの追加。

--- DBConnector6/src/main/java/tools/dbconnector6/App.java
+++ DBConnector6/src/main/java/tools/dbconnector6/App.java
@@ -3,7 +3,14 @@ package tools.dbconnector6;
 import javafx.application.Application;
 import tools.dbconnector6.controller.MainController;
 
+/**
+ * ツールのローンチクラス。<br>
+ */
 public class App {
+    /**
+     * 唯一のエントリポイント。<br>
+     * @param args 実行時引数
+     */
     public static void main( String[] args ) {
         Application.launch(MainController.class, args);
     }
