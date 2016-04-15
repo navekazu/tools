783dac8585266d913cccefe396a3738ed91a2e27
navekazu <navekazu0927@gmail.com>
2016-04-15 15:34:20 +0900

    docコメントを追加。

--- DBConnector6/src/main/java/tools/dbconnector6/transfer/ResultDataTransfer.java
+++ DBConnector6/src/main/java/tools/dbconnector6/transfer/ResultDataTransfer.java
@@ -7,15 +7,22 @@ import tools.dbconnector6.queryresult.QueryResultCellValue;
 import java.util.List;
 
 /**
- * 結果一覧の内容を転送する
+ * クエリ実行結果の内容を転送する抽象クラス。<br>
+ * 転送先は具象クラスで実装する。<br>
  */
 public abstract class ResultDataTransfer {
-    protected boolean evidenceMode;
-    protected boolean includeHeader;
-    protected String delimiter;
-    protected StringBuilder resultHeader;
-    protected StringBuilder resultData;
+    private boolean evidenceMode;           // エビデンス取得モード。クエリ実行結果をエビデンスとして転送する場合は true、それ以外は false
+    private boolean includeHeader;          // エビデンスにヘッダ（カラム）を含めるか？ 含める場合は true、それ以外は false
+    private String delimiter;               // カラムの区切り文字
+    private StringBuilder resultHeader;     // ヘッダの内容
+    private StringBuilder resultData;       // クエリ実行結果の内容
 
+    /**
+     * コンストラクタ
+     * @param evidenceMode エビデンス取得モード。クエリ実行結果をエビデンスとして転送する場合は true、それ以外は false
+     * @param includeHeader エビデンスにヘッダ（カラム）を含めるか？ 含める場合は true、それ以外は false
+     * @param delimiter カラムの区切り文字
+     */
     public ResultDataTransfer(boolean evidenceMode, boolean includeHeader, String delimiter) {
         this.evidenceMode = evidenceMode;
         this.includeHeader = includeHeader;
@@ -25,6 +32,11 @@ public abstract class ResultDataTransfer {
         this.resultData = new StringBuilder();
     }
 
+    /**
+     * ヘッダ内容を更新する。<br>
+     * エビデンス取得モードが true かつエビデンスにヘッダを含める場合のみ更新し、それ以外は何も行わない。
+     * @param header ヘッダ内容
+     */
     public void setHeader(List<TableColumn<QueryResult, String>> header) {
         if (!(evidenceMode && includeHeader)) {
             return;
@@ -36,6 +48,11 @@ public abstract class ResultDataTransfer {
         resultHeader.append("\n");
     }
 
+    /**
+     * クエリ実行結果を追記する。<br>
+     * エビデンス取得モードが true の場合のみ更新し、それ以外は何も行わない。
+     * @param list クエリ実行結果のレコード情報
+     */
     public void addData(List<QueryResultCellValue> list) {
         if (!evidenceMode) {
             return;
@@ -47,6 +64,10 @@ public abstract class ResultDataTransfer {
         resultData.append("\n");
     }
 
+    /**
+     * ヘッダ内容とクエリ実行結果を結合した文字列を返す。<br>
+     * @return ヘッダ内容とクエリ実行結果を結合した文字列
+     */
     protected String getTransferData() {
         StringBuilder data = new StringBuilder();
         data.append(resultHeader);
@@ -54,5 +75,17 @@ public abstract class ResultDataTransfer {
         return data.toString();
     }
 
+    /**
+     * エビデンス取得モードを返す。<br>
+     * @return エビデンス取得モード
+     */
+    protected boolean getEvidenceMode() {
+        return evidenceMode;
+    }
+
+    /**
+     * クエリ実行結果の転送処理。<br>
+     * 具体的な転送先は具象クラスで決定する。<br>
+     */
     public abstract void transfer();
 }
