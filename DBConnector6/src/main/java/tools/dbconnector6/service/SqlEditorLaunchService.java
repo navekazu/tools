9600239496b0e32a5640f2d72c1d8c8d0abb8533
navekazu <navekazu0927@gmail.com>
2016-04-19 20:23:52 +0900

    docコメント追加。

--- DBConnector6/src/main/java/tools/dbconnector6/service/SqlEditorLaunchService.java
+++ DBConnector6/src/main/java/tools/dbconnector6/service/SqlEditorLaunchService.java
@@ -7,6 +7,9 @@ import tools.dbconnector6.serializer.TemporaryQuerySerializer;
 
 import java.nio.file.Path;
 
+/**
+ * SQLエディタを立ち上げ、編集内容を受け取ってSQLクエリ編集領域にその編集内容を貼り付けるサービス。
+ */
 public class SqlEditorLaunchService implements BackgroundServiceInterface<Void, String> {
     // メイン画面へのアクセス用インターフェース
     private MainControllerInterface mainControllerInterface;
@@ -19,6 +22,12 @@ public class SqlEditorLaunchService implements BackgroundServiceInterface<Void, 
         this.mainControllerInterface = mainControllerInterface;
     }
 
+    /**
+     * バックグラウンドで実行する処理を実装する。<br>
+     * テンポラリファイルを作成し、SQLエディタでそのファイルを編集してもらい、結果をSQLクエリ編集領域に貼り付ける。<br>
+     * @param task 生成したバックグラウンド実行を行うTaskのインスタンス
+     * @throws Exception 何らかのエラーが発生し処理を中断する場合
+     */
     @Override
     public void run(Task task) throws Exception {
         if (mainControllerInterface.getEditorPath().trim().length()<=0) {
@@ -42,6 +51,12 @@ public class SqlEditorLaunchService implements BackgroundServiceInterface<Void, 
         update(temporaryQuerySerializer.readText(temporaryQueryPath));
     }
 
+    /**
+     * 更新の前処理。<br>
+     * 待機メッセージを表示<br>
+     * @param prepareUpdateParam 前処理に必要なパラメータ
+     * @throws Exception 何らかのエラーが発生した場合
+     */
     @Override
     public void prepareUpdate(final Void prepareUpdateParam) throws Exception {
         Platform.runLater(new Runnable() {
@@ -54,6 +69,12 @@ public class SqlEditorLaunchService implements BackgroundServiceInterface<Void, 
         });
     }
 
+    /**
+     * 更新処理。<br>
+     * 編集結果をSQLクエリ編集領域に貼り付ける。<br>
+     * @param updateParam 編集結果
+     * @throws Exception 何らかのエラーが発生した場合
+     */
     @Override
     public void update(final String updateParam) throws Exception {
         Platform.runLater(new Runnable() {
@@ -64,26 +85,44 @@ public class SqlEditorLaunchService implements BackgroundServiceInterface<Void, 
             }
         });
     }
+
+    /**
+     * バックグラウンド実行をキャンセルするたびに呼び出される。<br>
+     * 待機メッセージを非表示にする。<br>
+     */
     @Override
     public void cancel() {
         callHideWaitDialog();
     }
 
+    /**
+     * Serviceの状態がCANCELLED状態に遷移するたびに呼び出される。<br>
+     * 待機メッセージを非表示にする。<br>
+     */
     @Override
     public void cancelled() {
         callHideWaitDialog();
     }
 
+    /**
+     * Serviceの状態がFAILED状態に遷移するたびに呼び出される。<br>
+     * 待機メッセージを非表示にする。<br>
+     */
     @Override
     public void failed() {
         callHideWaitDialog();
     }
 
+    /**
+     * もし実行中ではない時にキャンセル要求があった場合のメッセージ。<br>
+     * @return メッセージ
+     */
     @Override
     public String getNotRunningMessage() {
         return "";
     }
 
+    // 待機メッセージを非表示にする。
     private void callHideWaitDialog() {
         Platform.runLater(new Runnable() {
             @Override
