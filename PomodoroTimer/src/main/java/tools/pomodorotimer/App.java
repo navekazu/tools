f0cce81b978727989a74fc15337f36e444d82f90
navekazu <navekazu0927@gmail.com>
2016-04-26 18:17:17 +0900

    ガワを実装。

--- PomodoroTimer/src/main/java/tools/pomodorotimer/App.java
+++ PomodoroTimer/src/main/java/tools/pomodorotimer/App.java
@@ -1,13 +1,38 @@
 package tools.pomodorotimer;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.*;
+
+public class App extends Application implements Initializable {
+    @FXML Label timerLabel;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Pomodoro timer");
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        timerLabel.setText("bbbb");
+    }
+
+    public static void main( String[] args ) {
+        Application.launch(App.class, args);
     }
 }
