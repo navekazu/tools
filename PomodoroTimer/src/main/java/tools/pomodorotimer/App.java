1895691c31f5c78527fb0f3c1bcefd4004680ea3
navekazu <navekazu0927@gmail.com>
2016-04-26 17:06:11 +0900

    PomodoroTimerプロジェクトの作成

---
+++ PomodoroTimer/src/main/java/tools/pomodorotimer/App.java
@@ -0,0 +1,13 @@
+package tools.pomodorotimer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ PomodoroTimer/src/test/java/tools/pomodorotimer/AppTest.java
@@ -0,0 +1,22 @@
+package tools.pomodorotimer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+}
