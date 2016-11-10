0e55acecd52335bd722894f935a74b94eb582bea
navekazu <navekazu0927@gmail.com>
2016-11-10 15:19:33 +0900

    mainの実装。

--- JavaTemplateGenerator/src/main/java/tools/templategenerator/App.java
+++ JavaTemplateGenerator/src/main/java/tools/templategenerator/App.java
@@ -1,9 +1,26 @@
 package tools.templategenerator;
 
+import java.io.IOException;
+
 public class App {
 
 
     public static void main(String[] args) {
+        if (args.length!=3) {
+            throw new IllegalArgumentException();
+        }
 
+        GeneratorParam param = GeneratorParam.builder()
+                .currentPath(args[0])
+                .projectName(args[1])
+                .packageName(args[2])
+                .build();
+
+        try {
+            Generator generator = new Generator();
+            generator.generate(param);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
