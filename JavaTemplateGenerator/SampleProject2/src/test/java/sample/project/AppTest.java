491173365a15810e45d4e24df7f15de9c6a708cf
navekazu <navekazu0927@gmail.com>
2016-12-04 20:26:04 +0900

    Selenideのサンプル作成中。

---
+++ JavaTemplateGenerator/SampleProject2/src/test/java/sample/project/AppTest.java
@@ -0,0 +1,28 @@
+package sample.project;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+    }
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void test() throws Exception {
+        assertTrue(true);
+    }
+}
