a6130036efb772b3d54359a2f16a48a1f1bfd5de
navekazu <navekazu0927@gmail.com>
2017-01-16 11:37:44 +0900

    CLIパラメータパーサー出来た。

--- EncryptTool/src/main/java/tools/cli/CLIParameterParser.java
+++ EncryptTool/src/main/java/tools/cli/CLIParameterParser.java
@@ -14,12 +14,40 @@ public class CLIParameterParser {
     static CLIParameters parse(List<String> optionPrefixes, CLIParameterRule rule, String... args) {
         Map<String, String> options = new HashMap<>();
         List<String> operands = new ArrayList<>();
+        String option = null;
+        boolean needValueFlag = false;
 
         // parse options
         for (int i=0; i<args.length; i++) {
-//            if (args[i].startsWith(optionPrefixes)) {
+            final String arg = args[i];
 
-//            }
+            if (needValueFlag) {
+                options.put(option, arg);
+                option = null;
+                needValueFlag = false;
+
+            } else {
+                Optional<String> optionPrefix = optionPrefixes.stream()
+                        .filter(s -> arg.startsWith(s))
+                        .findFirst();
+                if (optionPrefix.isPresent()) {
+                    // オプションが指定されている
+                    final String optionParameter = arg.substring(optionPrefix.get().length());
+
+                    // 値が必要？
+                    if (rule.needValueParameters.stream().anyMatch(s -> s.equals(optionParameter))) {
+                        option = arg.substring(optionPrefix.get().length());
+                        needValueFlag = true;
+                    } else {
+                        // 値は不要？（その場合はoptionOnlyParametersの部分一致でOK）
+                        rule.optionOnlyParameters.stream()
+                                .filter(s -> optionParameter.indexOf(s)!=-1)
+                                .forEach(s -> options.put(s, null));
+                    }
+                } else {
+                    operands.add(arg);
+                }
+            }
         }
 
         return new CLIParameters(options, operands);
