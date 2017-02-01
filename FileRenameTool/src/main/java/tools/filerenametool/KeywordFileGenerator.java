a315f949ec81f330336e1dc8740659afa08180dd
navekazu <navekazu0927@gmail.com>
2017-02-01 13:10:36 +0900

    キーワードファイルを生成する処理を追加中。

--- FileRenameTool/src/main/java/tools/filerenametool/KeywordFileGenerator.java
+++ FileRenameTool/src/main/java/tools/filerenametool/KeywordFileGenerator.java
@@ -3,4 +3,7 @@ package tools.filerenametool;
 public class KeywordFileGenerator {
     public static void main(String[] args) {
     }
+
+    public void generate(String srcFile, String destFile) {
+    }
 }
--- FileRenameTool/src/test/java/tools/filerenametool/KeywordFileGeneratorTest.java
+++ FileRenameTool/src/test/java/tools/filerenametool/KeywordFileGeneratorTest.java
@@ -2,7 +2,11 @@ package tools.filerenametool;
 
 import org.junit.*;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class KeywordFileGeneratorTest {
     @BeforeClass
@@ -22,7 +26,20 @@ public class KeywordFileGeneratorTest {
     }
 
     @Test
-    public void test() throws IOException {
-
+    public void generateTest() throws IOException {
+        try (BufferedWriter out = Files.newBufferedWriter(Paths.get("generateTest.txt"), Charset.forName("MS932"))) {
+            out.write("foo=bar");
+            out.newLine();
+            out.write("some=one");
+            out.newLine();
+            out.write("key=キーワード1");
+            out.newLine();
+            out.write("some=なにか");
+            out.newLine();
+            out.write("key=キーワード2");
+            out.newLine();
+        }
+        KeywordFileGenerator keywordFileGenerator = new KeywordFileGenerator();
+        keywordFileGenerator.generate("generateTest.txt", "keywordTest.txt");
     }
 }
