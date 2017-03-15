76ba3e16195f4f58cea0ced536fff632a4759fd2
navekazu <navekazu0927@gmail.com>
2017-03-15 13:28:07 +0900

    実行用のバッチを作成。

---
+++ EncryptTool/Encrypt.bat
@@ -0,0 +1,5 @@
+@rem create key file
+@rem java -classpath "%REPOSITORY_HOME%\tools\EncryptTool\build\libs\EncryptTool-1.0.jar" tools.encrypttool.App -k %*
+
+@rem encrypt
+java -classpath "%REPOSITORY_HOME%\tools\EncryptTool\build\libs\EncryptTool-1.0.jar" tools.encrypttool.App %*
---
+++ FileSplitter/FileSplitter.bat
@@ -0,0 +1,2 @@
+java -classpath "%REPOSITORY_HOME%\tools\FileSplitter\build\libs\FileSplitter-1.0.jar" tools.filesplitter.App -bat %*
+pause
