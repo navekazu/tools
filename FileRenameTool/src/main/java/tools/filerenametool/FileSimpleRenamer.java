66c6f2e753da7b78b1601f1aeeed3c8120c10caf
navekazu <navekazu0927@gmail.com>
2017-07-04 00:23:09 +0900

    ファイル名をシンプルに変換するクラス

--- FileRenameTool/src/main/java/tools/filerenametool/FileSimpleRenamer.java
+++ FileRenameTool/src/main/java/tools/filerenametool/FileSimpleRenamer.java
@@ -1,9 +1,14 @@
 package tools.filerenametool;
 
+import java.util.Map;
+
 public class FileSimpleRenamer {
+    private static Map<String, String> renameMap;
+
     public void exec() {
-    
+
     }
+
     public void main(String[] args) {
 
     }
