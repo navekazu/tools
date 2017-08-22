b70bcbe4916f5996d5d096ee1f0a9517f2f2b8bd
navekazu <navekazu0927@gmail.com>
2017-08-22 22:32:31 +0900

    重複名の変換

--- FileRenameTool/src/test/java/tools/filerenametool/FileSimpleRenamerTest.java
+++ FileRenameTool/src/test/java/tools/filerenametool/FileSimpleRenamerTest.java
@@ -9,7 +9,7 @@ public class FileSimpleRenamerTest {
     public void convertTest() throws Exception {
         FileSimpleRenamer r = new FileSimpleRenamer();
         assertEquals("a", r.convert("a"));
-        assertEquals("a", r.convert("a"));
+        assertEquals("Ａ", r.convert("A"));
 
     }
 
