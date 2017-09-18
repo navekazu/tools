ee649f0cb28567f6c42c89cd78e063725412a40b
navekazu <navekazu0927@gmail.com>
2017-09-19 07:39:12 +0900

    重複名の変換

--- FileRenameTool/src/main/java/tools/filerenametool/FileRenameSuite.java
+++ FileRenameTool/src/main/java/tools/filerenametool/FileRenameSuite.java
@@ -1,4 +1,22 @@
 package tools.filerenametool;
 
+import java.util.Arrays;
+
 public class FileRenameSuite {
+    private FileRenamerInterface[] fileRenamerInterfaces = new FileRenamerInterface[]{
+
+    };
+
+    public FileRenameSuite() {
+        execute();
+    }
+
+    private void execute() {
+        Arrays.asList(fileRenamerInterfaces)
+                .forEach(f -> f.toString());
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
