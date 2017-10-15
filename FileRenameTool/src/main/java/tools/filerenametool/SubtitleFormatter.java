bd41f98d93e2b35a0565edfa8aac287eb2d8cdc9
navekazu <navekazu0927@gmail.com>
2017-10-16 00:09:57 +0900

    重複名の変換

--- FileRenameTool/src/main/java/tools/filerenametool/SubtitleFormatter.java
+++ FileRenameTool/src/main/java/tools/filerenametool/SubtitleFormatter.java
@@ -3,6 +3,10 @@ package tools.filerenametool;
 import java.io.File;
 
 public class SubtitleFormatter implements FileRenamerInterface {
+    private String[] formatTarget = new String[] {
+
+    };
+
     @Override
     public String getName() {
         return null;
