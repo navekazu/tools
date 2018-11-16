d4e1a9ca167e177a3deccc36a4ca4078a2295bd3
k-watanabe <navekazu0927@gmail.com>
2018-11-16 18:45:19 +0900

    諸々

--- FileNameTransfer/src/main/java/tools/filenametransfer/FileNameTransfer.java
+++ FileNameTransfer/src/main/java/tools/filenametransfer/FileNameTransfer.java
@@ -40,7 +40,6 @@ public class FileNameTransfer {
 	}
 
 	public static void main(String[] args) throws IOException {
-		// TODO 自動生成されたメソッド・スタブ
 
 		FileNameTransfer fnt = new FileNameTransfer();
 		fnt.fromDirectory = args[0];
